28ddbe0b19015c2ebc95c65ac01e13206664df0e
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-01-29 22:27:03 +0100

    tmp

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
 var qs = require('./querystring')
 var cases = [
-  '&&',
-  '&=&='
+  /*
+  ['a=b&c&d=e', 'a=b&c=&d=e', { a: 'b', c: '', d: 'e' }],
+  ['a=b&c=&d=e', 'a=b&c=&d=e', { a: 'b', c: '', d: 'e' }],
+  ['a=b&=c&d=e', 'a=b&=c&d=e', { a: 'b', '': 'c', d: 'e' }],
+  ['a=b&=&c=d', 'a=b&=&c=d', { a: 'b', '': '', c: 'd' }],
+  */
+  ['&&&&', '', {}],
+  ['&&foo=bar&&', 'foo=bar', { foo: 'bar' }],
+  ['&=&=', '=&=', { '': [ '', '' ]}]
+  /*
+  ['&=&', '=', { '' : '' }],
+  */
 ]
 
 process.stdout.write('\033c')
+console.log(new Date() + '\n')
 
 cases.forEach(function (str) {
-  var out = qs.parse(str)
-  console.log(str + ': ', out)
-  console.log()
+  var out = qs.parse(str[0])
+  console.log(str[0] + ': ', out)
+  console.log(str[0] + ': ', str[2])
+  console.log('\n')
 })
